308a5d35f557191f1b97549ff72d4ae591bb64fe
anubis-robot <anubis@osiris.cyber.nyu.edu>
2022-02-10 20:29:12 +0000

    Anubis Cloud IDE Autosave netid=df1911

--- reverse_x_lines.c
+++ reverse_x_lines.c
@@ -7,8 +7,9 @@ char buf[512];
 // change it to is_palindrome functon
 // check whether x lines form a palindrome 
 void reverse_x_lines(int fd, char *name, int num_lines) {
-  int lines_read_length = 512;
-  int *lines_read = malloc(sizeof(char) * lines_read_length);
+  int lines_read_size = 512;
+  int lines_read_length 
+  int *lines_read = malloc(sizeof(char) * lines_read_size);
   lines_read = lines_read;
   int curr_lines_read = 0;
   int i, n;
@@ -16,6 +17,7 @@ void reverse_x_lines(int fd, char *name, int num_lines) {
 
   while ((n = read(fd, buf, sizeof(buf))) > 0) {
     for (i = 0; i < n; i++) {
+
       if (buf[i] == '\n')
         curr_lines_read++;
         // if (curr_lines_read == )
